972c9290f3cfc9eda13be17de51b85eb2eaef363
Alexander Poplavskiy <aleksandrpoplawskiy@gmail.com>
2021-08-18 22:09:02 +0300

    change home url

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  get 'bjelri', to: 'home#index'
+  get 'home', to: 'home#index'
 
   namespace :api do
     namespace :v1 do
