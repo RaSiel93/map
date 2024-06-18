00ea4fba6b840434d68f3bc4c5418e7e6c9d8d3b
rasiel <aleksandrpoplawskiy@gmail.com>
2024-06-18 21:54:11 +0300

    correct migration

--- db/migrate/20240614174160_fill_default_key_and_value_tags.rb
+++ db/migrate/20240614174160_fill_default_key_and_value_tags.rb
@@ -1,6 +1,6 @@
 class FillDefaultKeyAndValueTags < ActiveRecord::Migration[6.1]
   def up
-    TagKey.find_or_create_by(id: 0)
-    TagValue.find_or_create_by(id: 0)
+    tag_key = TagKey.find_or_create_by(id: 0)
+    TagValue.find_or_create_by(id: 0, tag_key: tag_key)
   end
 end
